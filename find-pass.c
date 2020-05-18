96b2796e67f2fe18d58110c50690535f9c9d3abe
Max Base <MaxBaseCode@Gmail.Com>
2020-05-18 17:01:49 +0430

    Create find-pass.c

---
+++ find-pass.c
@@ -0,0 +1,40 @@
+// Max Base
+// https://github.com/BaseMax/FindPasswordC
+#define a int
+#define b (
+#define xmjfigdfg jfdigjweoirwjeiorwer
+#define c )
+#define d {
+#define xdfgiozdf8ieo ncjvnxm,.mwoekrwheor
+#define e }
+#define f =
+#define xdfjgisdojfio wrwjeirowjeiro2y7huklxczs
+#define g ,
+#define h ;
+#define xdfkgiesutjioer szdnfjsdofhui32rhjkdvsv
+#define i if
+#define j else
+#define xdfkgopwuii2ewhdO sdfnhjweru23hroildfds
+#define k cout
+#define x jdifgdjfiogjixdfjgeof
+#define xzdfgioawehfjmcvm 23jru2o34huionlksdv
+#define l cin
+#define xdfsdjfisodjf sdvnjdsknui3w4rhy72ufhndvkldsv
+#define z erjtierjtierte
+#define xdfmgkjksdofpsdkofp sdhuiwy72384y327488q3274283
+#define m break;
+#define xerjiohncjklzx sdfndskvlnsdkvnsvkldfb
+#define n << endl
+#define xsdvmnkxlvnkxlc szfdnjkldsnskldgnklsgnksldf
+#define o =
+#define oo ==
+#define xfmvlcmvklcv dfngjdfgndfgdfdfg
+#define p string
+#define r while
+#define xfmvlcmvklcvsdmnsdvxcv dr82934u29384234fngjdfgndfgdfdfg
+#define t <<
+#define y >>
+#define xcjivoiwjeif 23y748263478
+#include <iostream>
+using namespace std h
+main b c d p password o d 48 g 49 e h p name o "Niyoosha" h password o "\x48\x66\x4f\x46\x4b\x46\x53\x31\x47\x35\x46\x36\x44\x46\x47\x44\x35\x46\x36" h p _ h r b true c d p q o password+"\x40\x21\x3f\x6d\x69\x73\x73" h k t "Enter system password: " h l y _ h i b q oo _ c d k t "Welcome " t name n h m e j d k t "Error: wrong password!" n h e e e
