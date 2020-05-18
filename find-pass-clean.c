6792a103c61e83d0e5ccc4211aa5ca59a9a4eb7e
Max Base <MaxBaseCode@Gmail.Com>
2020-05-18 17:02:54 +0430

    Update find-pass-clean.c

--- find-pass-clean.c
+++ find-pass-clean.c
@@ -1,2 +1,21 @@
 // Max Base
 // https://github.com/BaseMax/FindPasswordC
+#include <iostream>
+using namespace std;
+int main() {
+  string password="01";
+  string name="Name";
+  string p;
+  while(true) {
+    cout << "Enter system password: ";
+    cin >> p;
+    if(p == password) {
+      cout << "Welcome " << name << endl;
+      break;
+    }
+    else {
+      cout << "Error: wrong password!" << endl;
+    }
+  }
+  return 0;
+}
