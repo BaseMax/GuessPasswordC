7db33b1f7b3dc60341e468e61ae683f719cec3af
Max Base <MaxBaseCode@Gmail.Com>
2020-05-18 17:05:39 +0430

    Update variable-generator.php

--- variable-generator.php
+++ variable-generator.php
@@ -1,5 +1,5 @@
 <?php
-// it's only to create many variable as spam
+// it's only create many variable as spam
 for($i=0; $i < 500; $i++) {
   print 'int passwordi'.$i.' = '.($i * 100).';char passwords'.$i.'[10] = {\''.($i%7 * 1).'\',\''.($i%7 +1).'\',\''.($i%7 +2).'\',\''.($i%7 +3).'\'};';
 }
