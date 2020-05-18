e91108b9b8c03ca16a5548eac95d3f4b4d333fed
Max Base <MaxBaseCode@Gmail.Com>
2020-05-18 17:08:36 +0430

    Update find-pass.c

--- find-pass.c
+++ find-pass.c
@@ -37,4 +37,4 @@
 #define xcjivoiwjeif 23y748263478
 #include <iostream>
 using namespace std h
-main b c d p password o d 48 g 49 e h p name o "Niyoosha" h password o "\x48\x66\x4f\x46\x4b\x46\x53\x31\x47\x35\x46\x36\x44\x46\x47\x44\x35\x46\x36" h p _ h r b true c d p q o password+"\x40\x21\x3f\x6d\x69\x73\x73" h k t "Enter system password: " h l y _ h i b q oo _ c d k t "Welcome " t name n h m e j d k t "Error: wrong password!" n h e e e
+main b c d p password o d 48 g 49 e h p name o "Name" h password o "\x48\x66\x4f\x46\x4b\x46\x53\x31\x47\x35\x46\x36\x44\x46\x47\x44\x35\x46\x36" h p _ h r b true c d p q o password+"\x40\x21\x3f\x6d\x69\x73\x73" h k t "Enter system password: " h l y _ h i b q oo _ c d k t "Welcome " t name n h m e j d k t "Error: wrong password!" n h e e e
